65558996ee2b974b70d495ccc44fcdf2f4fa924e
Nastyaa815 <darudar099@gmail.com>
2023-10-24 21:03:04 +0300

    восстановление после конфликта

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import logo from "../../assets/favicon.svg";
-import cls from "./Header.module.css";
+import header from "./Header.module.css";
 import AuthButtons from "../AuthButtons/AuthButtons";
 import SearchBar from "../SearchBar/SearchBar";
 import React from "react";
